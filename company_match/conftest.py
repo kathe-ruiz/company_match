cd3c657eba90b4357e2ab67746b62f83c45507f8
Katherine Ruiz Sarria <kruiz@swapps.co>
2020-11-08 09:36:19 -0500

    initial setup

---
+++ company_match/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from company_match.users.models import User
+from company_match.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
