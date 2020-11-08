23234b375703665a5a3fbbb4204d263761d48811
Katherine Ruiz Sarria <kruiz@swapps.co>
2020-11-08 13:43:02 -0500

    add person searching

---
+++ company_match/match/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class userBioForm(forms.Form):
+    username = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    class Meta:
+        fields = ["username"]
--- company_match/match/views.py
+++ company_match/match/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 
+from .forms import userBioForm
+from .torreIntegration import TorreIntegration
 
-class HomeMatch(TemplateView):
+class HomeMatch(FormView):
     template_name = "match.html"
+    form_class = userBioForm
+    success_url = reverse_lazy("match:match")
+
+    def get(self, request, *args, **kwargs):
+        """Handle GET requests: instantiate a blank version of the form."""
+        return self.render_to_response(self.get_context_data())
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = self.request.GET.get("username")
+        if username is not None:
+            torreIntegration = TorreIntegration()
+            bio = torreIntegration.get_bio(username)
+            context["bio"] = bio.json()["person"]
+        return context
